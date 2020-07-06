17ded093c6841afd03eaf7c555395d3cda3fe51a
Ejaz Ahmed <ejazahmed.dev@gmail.com>
2020-07-06 20:48:08 +0500

    Removed unused imports

--- src/App.js
+++ src/App.js
@@ -1,7 +1,6 @@
 import React from "react";
 import Router from "./router";
 import Layout from "./components/Layout";
-import theme from "./theme";
 import { Client as Styletron } from "styletron-engine-atomic";
 import { Provider as StyletronProvider } from "styletron-react";
 import { LightTheme, BaseProvider } from "baseui";
