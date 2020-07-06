5ad50ec96cb86fe9183b619387caaffc00ba95dc
Ejaz Ahmed <ejazahmed.dev@gmail.com>
2020-07-06 19:21:11 +0500

    Added baseui

--- src/App.js
+++ src/App.js
@@ -1,12 +1,21 @@
 import React from "react";
 import Router from "./router";
 import Layout from "./components/Layout";
+import theme from "./theme";
+import { Client as Styletron } from "styletron-engine-atomic";
+import { Provider as StyletronProvider } from "styletron-react";
+import { LightTheme, BaseProvider } from "baseui";
+const engine = new Styletron();
 
 function App() {
   return (
-    <Layout>
-      <Router />
-    </Layout>
+    <StyletronProvider value={engine}>
+      <BaseProvider theme={LightTheme}>
+        <Layout>
+          <Router />
+        </Layout>
+      </BaseProvider>
+    </StyletronProvider>
   );
 }
 
--- src/pages/index.jsx
+++ src/pages/index.jsx
@@ -1,11 +1,12 @@
 import React from "react";
 import { Link } from "react-router-dom";
+import { Button } from "baseui/button";
 
 const Home = () => {
   return (
     <div>
-      Home
       <div>
+        <Button type="button">Hello World</Button>
         <Link to="/shoe"> Hello </Link>
       </div>
     </div>
