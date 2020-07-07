63a2cff38efe3314e82976b649a8b423284fca89
Ejaz Ahmed <ejazahmed.dev@gmail.com>
2020-07-07 15:01:44 +0500

    Added new theme object

--- src/components/Layout/Navbar/index.jsx
+++ src/components/Layout/Navbar/index.jsx
@@ -1,48 +1,50 @@
-import React from "react";
+import React from "react"
 import {
-  HeaderNavigation,
-  ALIGN,
-  StyledNavigationList,
-  StyledNavigationItem,
-} from "baseui/header-navigation";
-import { StyledLink } from "baseui/link";
-import { Button } from "baseui/button";
-import { useStyletron } from "baseui";
-import { MdShoppingCart } from "react-icons/md";
-import { Link } from "react-router-dom";
+    HeaderNavigation,
+    ALIGN,
+    StyledNavigationList,
+    StyledNavigationItem,
+} from "baseui/header-navigation"
+import { StyledLink } from "baseui/link"
+import { Button } from "baseui/button"
+import { useStyletron } from "baseui"
+import { MdShoppingCart } from "react-icons/md"
+import { Link } from "react-router-dom"
 
 const Navbar = ({ setIsOpen }) => {
-  const [css, theme] = useStyletron();
-  return (
-    <HeaderNavigation className={css({ padding: "0 1rem" })}>
-      <StyledNavigationList $align={ALIGN.left}>
-        <StyledNavigationItem>
-          <Link to="/">Uber</Link>
-        </StyledNavigationItem>
-      </StyledNavigationList>
-      <StyledNavigationList $align={ALIGN.left}>
-        <StyledNavigationItem>
-          <Link to="/shoe">Shoes</Link>
-        </StyledNavigationItem>
-        <StyledNavigationItem>
-          <StyledLink href="#basic-link2">Tab Link Two</StyledLink>
-        </StyledNavigationItem>
-      </StyledNavigationList>
-      <StyledNavigationList $align={ALIGN.center} />
-      <StyledNavigationList $align={ALIGN.right}>
-        <StyledNavigationItem>
-          <Button
-            $style={{ backgroundColor: theme.colors.accent }}
-            onClick={() => setIsOpen(true)}
-            kind="primary"
-            shape="round"
-          >
-            <MdShoppingCart size="1.3rem" />
-          </Button>
-        </StyledNavigationItem>
-      </StyledNavigationList>
-    </HeaderNavigation>
-  );
-};
+    const [css, theme] = useStyletron()
+    return (
+        <HeaderNavigation className={css({ padding: "0 1rem" })}>
+            <StyledNavigationList $align={ALIGN.left}>
+                <StyledNavigationItem>
+                    <Link to="/">
+                        <StyledLink animateUnderline={true}>Home</StyledLink>
+                    </Link>
+                </StyledNavigationItem>
+            </StyledNavigationList>
+            <StyledNavigationList $align={ALIGN.left}>
+                <StyledNavigationItem>
+                    <Link to="/shoe">Shoes</Link>
+                </StyledNavigationItem>
+                <StyledNavigationItem>
+                    <StyledLink href="#basic-link2">Tab Link Two</StyledLink>
+                </StyledNavigationItem>
+            </StyledNavigationList>
+            <StyledNavigationList $align={ALIGN.center} />
+            <StyledNavigationList $align={ALIGN.right}>
+                <StyledNavigationItem>
+                    <Button
+                        $style={{ backgroundColor: theme.colors.accent }}
+                        onClick={() => setIsOpen(true)}
+                        kind="primary"
+                        shape="round"
+                    >
+                        <MdShoppingCart size="1.3rem" />
+                    </Button>
+                </StyledNavigationItem>
+            </StyledNavigationList>
+        </HeaderNavigation>
+    )
+}
 
-export default Navbar;
+export default Navbar
--- src/theme.js
+++ src/theme.js
@@ -1,9 +1,27 @@
-// theme.js
-export default {
+const { createTheme } = require("baseui");
+const primitives = {
+  accent: "#F127E4", // hot pink
+  accent50: "#FDEDFC",
+  accent100: "#FCD3F9",
+  accent200: "#F89FF3",
+  accent300: "#F45AEA",
+  accent400: "#F127E4",
+  accent500: "#B71DAD",
+  accent600: "#901788",
+  accent700: "#600F5B",
+};
+
+const overrides = {
   colors: {
-    black: "#000e1a",
-    white: "#fff",
-    blue: "#007ce0",
-    navy: "#004175",
+    buttonSecondaryFill: primitives.accent100,
+    buttonSecondaryText: primitives.accent,
+    buttonSecondaryHover: primitives.accent200,
+    buttonSecondaryActive: primitives.accent300,
+    buttonSecondarySelectedFill: primitives.accent200,
+    buttonSecondarySelectedText: primitives.accent,
+    buttonSecondarySpinnerForeground: primitives.accent700,
+    buttonSecondarySpinnerBackground: primitives.accent300,
   },
 };
+
+export const theme = createTheme(primitives, overrides);
