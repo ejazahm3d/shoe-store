e3b8f626cdd8dab1bf5f2ed2f388921c179640de
Ejaz Ahmed <ejazahmed.dev@gmail.com>
2020-07-08 21:37:49 +0500

    Added product page

--- src/pages/Product.jsx
+++ src/pages/Product.jsx
@@ -1,12 +1,69 @@
 import React from "react"
 import { useParams } from "react-router-dom"
 import { useSelector } from "react-redux"
-
+import "react-responsive-carousel/lib/styles/carousel.min.css" // requires a loader
+import { Carousel } from "react-responsive-carousel"
+import { Container, Row, Col } from "react-grid-system"
+import { HeadingMedium, HeadingLarge, Paragraph1 } from "baseui/typography"
+import { Button } from "baseui/button"
 const Product = () => {
     const params = useParams()
     const state = useSelector((state) => state.products)
-
-    return <div>Product</div>
+    const productsCategoryName = params?.category
+    const productId = params?.product
+    const products = state?.[productsCategoryName]?.products
+    const currentProduct = products?.[productId]
+    const images = currentProduct?.images ?? []
+    console.log(images)
+    return (
+        <Container>
+            <div style={{ marginTop: "2rem" }}>
+                <Row>
+                    <Col xs={12} sm={12} md={12} lg={5}>
+                        <Carousel
+                            showStatus={false}
+                            showArrows
+                            showIndicators={false}
+                            dynamicHeight={true}
+                        >
+                            {images.map((img, index) => (
+                                <div key={img} index={index}>
+                                    <img
+                                        style={{ width: "100%" }}
+                                        src={img}
+                                        alt=""
+                                    />
+                                </div>
+                            ))}
+                        </Carousel>
+                    </Col>
+                    <Col xs={12} sm={12} md={12} lg={7}>
+                        <HeadingLarge marginTop="0.5rem" marginBottom="1rem">
+                            {currentProduct.name}
+                        </HeadingLarge>
+                        <HeadingMedium marginTop="0">
+                            Price:{" "}
+                            <span style={{ color: "red" }}>
+                                {currentProduct.price}
+                            </span>
+                        </HeadingMedium>
+                        <Paragraph1
+                            dangerouslySetInnerHTML={{
+                                __html: currentProduct.description,
+                            }}
+                        ></Paragraph1>
+                        <Button
+                            shape="square"
+                            size="large"
+                            $style={{ marginTop: "2rem" }}
+                        >
+                            Add to Cart
+                        </Button>
+                    </Col>
+                </Row>
+            </div>
+        </Container>
+    )
 }
 
 export default Product
