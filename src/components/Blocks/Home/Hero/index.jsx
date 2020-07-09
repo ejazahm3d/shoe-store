3ba981131e1042773cfe96e88c610637c83f05d0
Ejaz Ahmed <ejazahmed.dev@gmail.com>
2020-07-09 19:38:20 +0500

    Changed home page pic

--- src/components/Blocks/Home/Hero/index.jsx
+++ src/components/Blocks/Home/Hero/index.jsx
@@ -1,6 +1,5 @@
 import React from "react"
 import { useStyletron } from "baseui"
-import { Display2 } from "baseui/typography"
 
 const Hero = () => {
     const [css] = useStyletron()
@@ -11,31 +10,13 @@ const Hero = () => {
                 position: "relative",
             })}
         >
-            <Display2
-                color="white"
-                className={css({
-                    textAlign: "center",
-                    fontFamily: "mono",
-                    position: "absolute",
-                    fontWeight: "bold",
-                    left: 0,
-                    right: 0,
-                    bottom: "5%",
-                    marginLeft: "auto",
-                    marginRight: "auto",
-                    width: "100%",
-                })}
-            >
-                JUST DO IT!
-            </Display2>
             <img
                 className={css({
                     width: "100%",
                     objectFit: "cover",
                     height: "40rem",
-                    objectPosition: "65% 80%",
                 })}
-                src="/hero-image.jpg"
+                src="/nike-hero.jpg"
                 alt=""
             />
         </div>
